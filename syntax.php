14d1d2571057dbe135b4823bacd33b563ca51446
nefercheprure <hidden@>
2012-06-03 02:59:33 +0200

    works well, except for headers

--- syntax.php
+++ syntax.php
@@ -17,17 +17,17 @@ require_once DOKU_PLUGIN.'syntax.php';
 
 class syntax_plugin_divstyletag extends DokuWiki_Syntax_Plugin {
 	public function getType() {
-		//return 'FIXME: container|baseonly|formatting|substition|protected|disabled|paragraphs';
 		return 'container';
 	}
 
 	public function getPType() {
-		//return 'FIXME: normal|block|stack';
 		return 'normal';
 	}
 
 	public function getSort() {
-		return 12453;
+		// shall be < code(200)
+		// shall be > header(50)
+		return 158;
 	}
 
 	/**
@@ -39,24 +39,26 @@ class syntax_plugin_divstyletag extends DokuWiki_Syntax_Plugin {
 	 */
 	function getAllowedTypes() {
 		global $PARSER_MODES;
-
-		$rv = array_merge (
-			$PARSER_MODES['container'],
-			$PARSER_MODES['baseonly'],
-			$PARSER_MODES['paragraphs'],
-			$PARSER_MODES['formatting'],
-			$PARSER_MODES['substition'],
-			$PARSER_MODES['protected'],
-			$PARSER_MODES['disabled']
+		$rv = array(
+			'container','substition','protected','disabled','formatting','paragraphs' /*
+			'container','substition','protected','disabled','formatting','paragraphs',
+			'baseonly' // */
 		);
- 
 		return $rv;
 	}
 
+	// override default accepts() method to allow nesting
+	// - ie, to get the plugin accepts its own entry syntax
+	function accepts($mode)
+	{
+		if ($mode == substr(get_class($this), 7)) return true;
+		return parent::accepts($mode);
+	}
 
 	public function connectTo($mode) {
-		//$this->Lexer->addSpecialPattern('<FIXME>',$mode,'plugin_divstyletag');
-		$this->Lexer->addEntryPattern('<div(?:(?: +style="(?:[^"<>]*)")?(?: +class="(?:[^"<>]*)"))?>(?=.*?</div>)',$mode,'plugin_divstyletag');
+		//FIXME: add pseudoheaders
+		$this->Lexer->addSpecialPattern( '[ \t]*={2,}[^\n]+={2,}[ \t]*(?=\n)',$mode,'plugin_divstyletag');
+		$this->Lexer->addEntryPattern('<div(?:(?: +style="(?:[^"<>]*)")?(?: +class="(?:[^"<>]*)")?)>(?=.*?</div>)',$mode,'plugin_divstyletag');
 	}
 
 	public function postConnect() {
@@ -71,14 +73,28 @@ class syntax_plugin_divstyletag extends DokuWiki_Syntax_Plugin {
 				$val = $match;
 			}
 			return array($state, $val);
-
+		case DOKU_LEXER_MATCHED:
+			/*
+			$sec = $handler->status['section'];
+			$handler->status['section'] = false;
+			$handler->header($match,$state,$pos);
+			$handler->status['section'] = $sec;
+			$handler->_addCall('section_close', array(), $pos);
+			// */
+			$title = trim($match);
+			$level = 7 - strspn($title,'=');
+			if($level < 1) $level = 1;
+			$title = trim($title,'=');
+			$title = trim($title);
+			return array($state, array($title, $level, $pos));
 		case DOKU_LEXER_UNMATCHED:
 			$handler->_addCall('cdata', array($match), $pos);
-			return array($state,'');
+			return false; 
+			//return array($state,$match);
 		case DOKU_LEXER_EXIT:
 			return array($state, '');
 		}
-		return array();
+		return false;//array();
 	}
 
 	public function render($mode, &$renderer, $data) {
@@ -87,8 +103,34 @@ class syntax_plugin_divstyletag extends DokuWiki_Syntax_Plugin {
 			switch ($state) {
 			case DOKU_LEXER_ENTER:
 				$renderer->doc .= $val;
+				break;
 			case DOKU_LEXER_EXIT:
 				$renderer->doc .= '</div>';
+				break;
+			case DOKU_LEXER_MATCHED:
+				list($text,$level,$pos) = $val;
+				if(!$text) break; //skip empty headlines
+
+				$hid = $renderer->_headerToLink($text,true);
+
+				//only add items within configured levels
+				$renderer->toc_additem($hid, $text, $level);
+
+				// adjust $node to reflect hierarchy of levels
+				$renderer->node[$level-1]++;
+				if ($level < $renderer->lastlevel) {
+					for ($i = 0; $i < $renderer->lastlevel-$level; $i++) {
+						$renderer->node[$renderer->lastlevel-$i-1] = 0;
+					}
+				}
+				$renderer->lastlevel = $level;
+
+				// write the header
+				$renderer->doc .= DOKU_LF.'<h'.$level;
+				$renderer->doc .= '><a name="'.$hid.'" id="'.$hid.'">';
+				$renderer->doc .= $renderer->_xmlEntities($text);
+				$renderer->doc .= "</a></h$level>".DOKU_LF;
+				break;
 			}
 			return true;
 		}
